6e913089e9dde6833353a1b4688c0bc1e9587ca0
Vitalii Novik <novickvitaliy@gmail.com>
2024-07-29 13:32:08 +0300

    create page with tables

---
+++ Client/DatingAgencyMS.Client/Features/Tables/Pages/Tables.razor.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DatingAgencyMS.Client.Features.Tables.Pages;
+
+public partial class Tables : ComponentBase
+{
+}
--- Client/DatingAgencyMS.Client/obj/Debug/net8.0/DatingAgencyMS.Client.AssemblyInfo.cs
+++ Client/DatingAgencyMS.Client/obj/Debug/net8.0/DatingAgencyMS.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatingAgencyMS.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eecce5dd0f4b4290b50c374bdd14d70e539e53bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dafefd9bb4994d9f6f8fe0deeb02dfe48de2807")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatingAgencyMS.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatingAgencyMS.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
