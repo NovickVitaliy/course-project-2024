75f9b5aa99c68f58a22a35ae619560d5cfda7185
Vitalii Novik <novickvitaliy@gmail.com>
2024-08-07 07:51:17 +0300

    create table card component

---
+++ Client/DatingAgencyMS.Client/Features/Tables/Components/TableCard.razor.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components;
+
+namespace DatingAgencyMS.Client.Features.Tables.Components;
+
+public partial class TableCard : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public string TableName { get; init; }
+    
+    [Parameter, EditorRequired]
+    public string TableLink { get; init; }
+}
--- Client/DatingAgencyMS.Client/obj/Debug/net8.0/DatingAgencyMS.Client.AssemblyInfo.cs
+++ Client/DatingAgencyMS.Client/obj/Debug/net8.0/DatingAgencyMS.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatingAgencyMS.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bfbae525b60fd322883acf610ccfca0e6856cd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce0d3d7a748e685aeed925522bed421d3b835d14")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatingAgencyMS.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatingAgencyMS.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
