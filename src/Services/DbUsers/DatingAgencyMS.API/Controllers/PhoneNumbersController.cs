4365b905fce902c40755205c318058bb3aa2340e
Vitalii Novik <novickvitaliy@gmail.com>
2024-11-02 15:31:45 +0200

    feat: implement controller for the PhoneNumbers

--- src/Services/DbUsers/DatingAgencyMS.API/Controllers/PhoneNumbersController.cs
+++ src/Services/DbUsers/DatingAgencyMS.API/Controllers/PhoneNumbersController.cs
@@ -26,4 +26,53 @@ public class PhoneNumbersController : BaseApiController
 
         return Created($"api/phone-numbers/{result.ResponseData}", new {id = result.ResponseData});
     }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAsync([FromQuery] GetPhoneNumbersRequest request)
+    {
+        var result = await _phoneNumbersService.GetPhoneNumbersAsync(request);
+        if (!result.Success)
+        {
+            return StatusCode(result.Code, result.ToHttpErrorResponse());
+        }
+
+        return Ok(result.ResponseData);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetByIdAsync(int id)
+    {
+        var result = await _phoneNumbersService.GetPhoneNumberByIdAsync(id);
+        if (!result.Success)
+        {
+            return StatusCode(result.Code, result.ToHttpErrorResponse());
+        }
+
+        return Ok(result.ResponseData);
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<IActionResult> UpdateAsync(int id, UpdatePhoneNumberRequest request)
+    {
+        request = request with { Id = id };
+        var result = await _phoneNumbersService.UpdatePhoneNumberAsync(request);
+        if (!result.Success)
+        {
+            return StatusCode(result.Code, result.ToHttpErrorResponse());
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _phoneNumbersService.DeletePhoneNumberAsync(id);
+        if (!result.Success)
+        {
+            return StatusCode(result.Code, result.ToHttpErrorResponse());
+        }
+
+        return NoContent();
+    }
 }
