afdf32f80081715e90653edc22df2c412fc7ed3f
Vitalii Novik <novickvitaliy@gmail.com>
2024-11-03 01:39:38 +0200

    feat: add page for updating the client rating

--- Client/DatingAgencyMS.Client/Features/ClientRatings/Pages/UpdateClientRating.razor.cs
+++ Client/DatingAgencyMS.Client/Features/ClientRatings/Pages/UpdateClientRating.razor.cs
@@ -10,29 +10,48 @@ using Refit;
 
 namespace DatingAgencyMS.Client.Features.ClientRatings.Pages;
 
-public partial class CreateClientRating : ComponentBase
+public partial class UpdateClientRating : ComponentBase
 {
+    [Parameter] public int Id { get; init; }
+    [Inject] private NavigationManager NavigationManager { get; init; } = null!;
     [Inject] private ToastService ToastService { get; init; } = null!;
     [Inject] private IClientRatingsService ClientRatingsService { get; init; } = null!;
     [Inject] private IState<UserState> UserState { get; init; } = null!;
-    [Inject] private NavigationManager NavigationManager { get; init; } = null!;
     private LoggedInUser? _loggedInUser = null;
-    private readonly CreateClientRatingRequest? _request = new();
-    
+    private UpdateClientRatingRequest? _request = new(); 
+        
     protected override void OnInitialized()
     {
         _loggedInUser = UserState.Value.User;
         UserState.StateChanged += (_, _) => _loggedInUser = UserState.Value.User;
         base.OnInitialized();
     }
+
     
+    protected override async Task OnParametersSetAsync()
+    {
+        if (_loggedInUser is not null)
+        {
+            var dto = await ClientRatingsService.GetByIdAsync(Id, UserState.Value.User!.Token);
+            _request = new UpdateClientRatingRequest()
+            {
+                Comment = dto.Comment,
+                Id = Id,
+                Rating = dto.Rating
+            };
+        }
+        
+        await base.OnParametersSetAsync();
+    }
+
     private async Task OnValidSubmit()
     {
         if (_request is null) return;
+
         try
         {
-            await ClientRatingsService.CreateAsync(_request, UserState.Value.User!.Token);
-            ToastService.Notify(new ToastMessage(ToastType.Success, "Відгук клієнта було успішно створено. Переводимо вас назад."));
+            await ClientRatingsService.UpdateAsync(Id, _request, _loggedInUser!.Token);
+            ToastService.Notify(new ToastMessage(ToastType.Success, "Відгук клієнта було успішно оновлено. Переводжу назад"));
             await Task.Delay(2000);
             NavigationManager.NavigateTo("/tables/client-ratings");
         }
